8c147f3a26aea92c1268beccb0d8d62b3304d8ac
Wallnussfolie <volkersedlmair@web.de>
2020-03-21 13:04:38 +0100

    changed welcome controller

---
+++ SurvCovid/src/main/java/org/hackathon/wirvswirus/thecouchdevs/SurvCovid/web/controller/WelcomeController.java
@@ -0,0 +1,18 @@
+package org.hackathon.wirvswirus.thecouchdevs.SurvCovid.web.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+	
+	@GetMapping("/index")
+	public String welcome(@RequestParam(name="name", required =false,defaultValue="World") String name, Model model) {
+		
+		return "Hello User";
+		
+	}
+
+}
