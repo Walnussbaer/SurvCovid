8db52bc9c60ecbecde807b5140810c54fb88b29c
Wallnussfolie <volkersedlmair@web.de>
2020-04-11 11:04:43 +0200

    added some changes for startup process from philipp

--- src/main/java/org/hackathon/wirvswirus/thecouchdevs/SurvCovid/SurvCovidApplication.java
+++ src/main/java/org/hackathon/wirvswirus/thecouchdevs/SurvCovid/SurvCovidApplication.java
@@ -8,11 +8,15 @@ import java.util.stream.Stream;
 import org.hackathon.wirvswirus.thecouchdevs.SurvCovid.data.entity.GameEvent;
 import org.hackathon.wirvswirus.thecouchdevs.SurvCovid.data.entity.GameEventChoice;
 import org.hackathon.wirvswirus.thecouchdevs.SurvCovid.data.entity.GameEventDefinition;
+import org.hackathon.wirvswirus.thecouchdevs.SurvCovid.data.entity.InventoryItem;
+import org.hackathon.wirvswirus.thecouchdevs.SurvCovid.data.entity.ItemType;
 import org.hackathon.wirvswirus.thecouchdevs.SurvCovid.data.entity.User;
 import org.hackathon.wirvswirus.thecouchdevs.SurvCovid.data.entity.enumeration.GameEventDefinitionType;
 import org.hackathon.wirvswirus.thecouchdevs.SurvCovid.game.logic.service.GameEventChoiceService;
 import org.hackathon.wirvswirus.thecouchdevs.SurvCovid.game.logic.service.GameEventDefinitionService;
 import org.hackathon.wirvswirus.thecouchdevs.SurvCovid.game.logic.service.GameEventService;
+import org.hackathon.wirvswirus.thecouchdevs.SurvCovid.game.logic.service.InventoryService;
+import org.hackathon.wirvswirus.thecouchdevs.SurvCovid.game.logic.service.ItemTypeService;
 import org.hackathon.wirvswirus.thecouchdevs.SurvCovid.game.logic.service.UserService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -32,9 +36,9 @@ public class SurvCovidApplication {
 	 * @param userService
 	 * @return
 	 */
-	@Bean CommandLineRunner createUserTestData(UserService userService) {
+	@Bean CommandLineRunner createUserTestData(UserService userService, ItemTypeService itemTypeService, InventoryService inventoryService) {
 		return args -> {
-			
+		    
 			System.out.println("Creating user test data");
 			
 			Stream.of("John","Peter","Max","Volker","Paul","Sharmin","Vroni","Philipp","Gino","Henning").forEach(name -> {
@@ -46,6 +50,47 @@ public class SurvCovidApplication {
 				System.out.println(user.getUserName());
 				//System.out.println(user.getUserId());
 			});
+			
+			////////// Testing inventory and items
+            // Adding user
+            System.out.println("Fetching one test user");
+            User user = userService.getUserByName("Philipp");
+
+            // Adding item type
+            System.out.println("Creating item types");
+            ItemType itemType1 = new ItemType(1, "Zeitung");
+            ItemType itemType2 = new ItemType(2, "Buch");
+            itemTypeService.addItem(itemType1);
+            itemTypeService.addItem(itemType2);
+
+            // List currently existing items
+            System.out.println("List all item types in the database:");
+            List<ItemType> testItemTypes = itemTypeService.getAllItemTypes();
+            for(ItemType i: testItemTypes)
+                System.out.println("  - (" + i.getItemTypeId() + ") " + i.getItemTypeName());
+
+            // Adding items of item types to the user's inventory
+            System.out.println("Adding items to the user's inventory");
+            InventoryItem inventoryItem1 = new InventoryItem(user, itemType1, 1);
+            inventoryService.saveInventoryItem(inventoryItem1);
+            InventoryItem inventoryItem2 = new InventoryItem(user, itemType2, 5);
+            inventoryService.saveInventoryItem(inventoryItem2);
+
+            // List items in user's inventory
+            System.out.println("List items of user " + user.getUserName() + "s inventory");
+            List<InventoryItem> userItems = inventoryService.getInventory("Philipp");
+            for(InventoryItem i: userItems)
+                System.out.println("User " + i .getUserName() + " has " + i.getItemCount() + " of " + i.getItemTypeName() + ".");
+
+            // Adding some more of one item type to the user's inventory
+            System.out.println("Adding some more of one item type to the user's inventory");
+            inventoryService.addItemCount(user, itemType1, 3);
+
+            // List items in user's inventory
+            System.out.println("List items of user " + user.getUserName() + "s inventory");
+            userItems = inventoryService.getInventory("Philipp");
+            for(InventoryItem i: userItems)
+                System.out.println("User " + i .getUserName() + " has " + i.getItemCount() + " of " + i.getItemTypeName() + ".");
 			
 			System.out.println("Finished creating user test data");
 		};
