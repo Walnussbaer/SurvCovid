cc61933c9a00f9989e970e9fe068312a83221bc6
Phiwa <mail@phiwa.eu>
2020-06-10 20:58:20 +0200

    Adjusted security settings to allow access to new api docs. Needs to be adjusted to improve security

--- src/main/java/org/hackathon/wirvswirus/thecouchdevs/SurvCovid/web/config/WebSecurityConfig.java
+++ src/main/java/org/hackathon/wirvswirus/thecouchdevs/SurvCovid/web/config/WebSecurityConfig.java
@@ -74,8 +74,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .antMatchers("/api/auth/**").permitAll() // / authentication endpoint does not need authorization
                     //TODO: discuss in team whether we want another layer in our URL only for security endpoints
                     .antMatchers("/swagger-ui/**").permitAll()  // TODO: add configuration parameter in application.properties to disable this in production mode
-                    .antMatchers("/**").permitAll()
-                    .antMatchers("/swagger-ui.html").permitAll()
+                    .antMatchers("/**").permitAll() // TODO: Properly configure whitelisting of /swagger-ui.html
                     .antMatchers("/v2/api-docs/**").permitAll()
                     .antMatchers("/v3/api-docs/**").permitAll() // TODO: add configuration parameter in application.properties to disable this in production mode
                     .anyRequest().authenticated();
