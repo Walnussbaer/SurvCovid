e0f1d61ab7107260cf12f88314878f01ae71f17b
Phiwa <mail@phiwa.eu>
2020-07-04 16:37:09 +0200

    Moving more test data generation to utils class

--- src/main/java/org/hackathon/wirvswirus/thecouchdevs/SurvCovid/game/utils/StartupUtils.java
+++ src/main/java/org/hackathon/wirvswirus/thecouchdevs/SurvCovid/game/utils/StartupUtils.java
@@ -4,6 +4,8 @@ import org.hackathon.wirvswirus.thecouchdevs.SurvCovid.data.entity.*;
 import org.hackathon.wirvswirus.thecouchdevs.SurvCovid.data.entity.enumeration.GameEventDefinitionType;
 import org.hackathon.wirvswirus.thecouchdevs.SurvCovid.data.entity.enumeration.RoleName;
 import org.hackathon.wirvswirus.thecouchdevs.SurvCovid.data.entity.response.GameState;
+import org.hackathon.wirvswirus.thecouchdevs.SurvCovid.game.logic.manager.GameManager;
+import org.hackathon.wirvswirus.thecouchdevs.SurvCovid.game.logic.manager.submanager.ShopManager;
 import org.hackathon.wirvswirus.thecouchdevs.SurvCovid.game.logic.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -36,6 +38,21 @@ public class StartupUtils {
 
     @Autowired
     private GameEventChoiceService gameEventChoiceService;
+
+    @Autowired
+    private ItemTypeService itemTypeService;
+
+    @Autowired
+    private InventoryService inventoryService;
+
+    @Autowired
+    private ShopService shopService;
+
+    @Autowired
+    private ShopItemService shopItemService;
+
+    @Autowired
+    private GameManager gameManager;
 
     /**
      * Create sample users for SurvCovid to test functionality. This method only creates normal players.
@@ -152,6 +169,80 @@ public class StartupUtils {
 
         System.out.println("Event test data got created!");
 
+    }
+
+    public void createSampleInventoryData() {
+        /* Testing inventory and items */
+        // Adding user
+        System.out.println("Fetching one test user");
+        User user = userService.getUserByName("Philipp")
+                .orElseThrow(() -> new RuntimeException("No user with name Philipp was found!"));
+
+        // Adding item type
+        System.out.println("Creating item types");
+        ItemType itemType1 = new ItemType(1, "newspaper", "Zeitung");
+        ItemType itemType2 = new ItemType(2, "book", "Buch");
+        itemTypeService.addItem(itemType1);
+        itemTypeService.addItem(itemType2);
+
+        // List currently existing items
+        System.out.println("List all item types in the database:");
+        List<ItemType> testItemTypes = itemTypeService.getAllItemTypes();
+        for(ItemType i: testItemTypes)
+            System.out.println("  - (" + i.getItemTypeId() + " / " + i.getItemTypeName() + ") => '" + i.getItemTypeDisplayName() + "'");
+
+        // Adding items of item types to the user's inventory
+        System.out.println("Adding items to the user's inventory");
+        InventoryItem inventoryItem1 = new InventoryItem(user, itemType1, 1);
+        inventoryService.saveInventoryItem(inventoryItem1);
+        InventoryItem inventoryItem2 = new InventoryItem(user, itemType2, 5);
+        inventoryService.saveInventoryItem(inventoryItem2);
+
+        // List items in user's inventory
+        System.out.println("List items of user " + user.getUserName() + "s inventory");
+        List<InventoryItem> userItems = inventoryService.getInventory(user.getUserId());
+        for(InventoryItem i: userItems)
+            System.out.println("User " + i .getUserName() + " has " + i.getItemCount() + " of " + i.getItemTypeDisplayName() + ".");
+
+        // Adding some more of one item type to the user's inventory
+        System.out.println("Adding some more of one item type to the user's inventory");
+        inventoryService.addItemCount(user, itemType1, 3);
+
+        // List items in user's inventory
+        System.out.println("List items of user " + user.getUserName() + "s inventory");
+        userItems = inventoryService.getInventory(user.getUserId());
+        for(InventoryItem i: userItems)
+            System.out.println("User " + i .getUserName() + " has " + i.getItemCount() + " of " + i.getItemTypeDisplayName() + ".");
+    }
+
+    public void createSampleShopData() {
+        ////////// Testing shops
+
+        System.out.println("Fetching one test user");
+        User user = userService.getUserByName("Philipp")
+                .orElseThrow(() -> new RuntimeException("No user with name Philipp was found!"));
+
+        // Create a shop
+        System.out.println("Testing Shop creation");
+        Shop userShop = new Shop(user);
+        userShop = shopService.saveShop(userShop);
+
+        // Get two item types to use for shop items
+        ItemType itemType1 = itemTypeService.getItemByTypeId(1);
+        ItemType itemType2 = itemTypeService.getItemByTypeId(2);
+
+        // Adding items to Shop
+        ShopItem shopItem1 = new ShopItem(userShop, itemType1, 5, 4.75);
+        shopItemService.saveShopItem(shopItem1);
+        ShopItem shopItem2 = new ShopItem(userShop, itemType2, 27, 19.99);
+        shopItemService.saveShopItem(shopItem2);
+
+        ShopManager shopManager = gameManager.getShopManager();
+        List<ShopItem> sortiment = shopManager.getOrCreateShopStock(user);
+
+        System.out.println("Shop of player " + user.getUserName() + " currently offers the following items:");
+        for(ShopItem ssi: sortiment)
+            System.out.println("- ItemType '" + ssi.getItemType().getItemTypeDisplayName() + "' / ItemCount: " + ssi.getItemCount() + " / ItemPrice: " + ssi.getItemPrice() + ".");
     }
 
 }
